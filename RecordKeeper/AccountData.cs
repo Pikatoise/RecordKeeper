fa889485396855ce98d336dd9a704ef0ae4db72c
Pikatoise <wierwolf@inbox.ru>
2022-06-10 20:16:42 +0500

    Добавил класс для аккаунтов

---
+++ RecordKeeper/AccountData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordKeeper
+{
+    internal class AccountData
+    {
+        struct Account
+        {
+            public string Login;
+            public string Password;
+            public string Access;
+        }
+
+        Account currentAccount;
+
+        List<Account> accounts = new List<Account>();
+
+        public AccountData()
+        {
+
+        }
+
+        void InitData()
+        {
+
+        }
+    }
+}
